bdf294b159783629e922ff8259c27266b89c5a7a
Mustafa Bulu <mustafabulu@hotmail.com>
2026-08-28 18:43:13 +0300

    Initial project setup

---
+++ src/main/java/com/mustafabulu/realtimefeatureplatform/RealtimeFeaturePlatformApplication.java
@@ -0,0 +1,13 @@
+package com.mustafabulu.realtimefeatureplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealtimeFeaturePlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RealtimeFeaturePlatformApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mustafabulu/realtimefeatureplatform/RealtimeFeaturePlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mustafabulu.realtimefeatureplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealtimeFeaturePlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
